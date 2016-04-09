45880547db2ad820ed6c2f5387f68dc5b264dab5
Josh <josh@Joshs-MacBook-Air.local>
2016-04-09 13:01:21 -0400

    Pulled enterprise data into /json folder

--- src/get_json.py
+++ src/get_json.py
@@ -8,10 +8,10 @@ import urllib
 customerId = 'your customerId here'
 apiKey = 'your apiKey here'
 
-url = 'http://api.reimaginebanking.com/enterprise/merchants?key=9353cf37bda3beedf35c7964ea1e1f3d'#.format(customerId,apiKey)
+url = 'http://api.reimaginebanking.com/enterprise/withdrawals?key=9353cf37bda3beedf35c7964ea1e1f3d'#.format(customerId,apiKey)
 
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 
-with open('data.txt', 'w') as outfile:
+with open('withdrawals.json', 'w') as outfile:
     json.dump(data, outfile)
--- src/test.js
+++
@@ -1,6 +0,0 @@
-$.ajax({
-    url: 'api.reimaginebanking.com/atms?key=your_key',
-    success: function(results){
-        console.log("test")
-    }
-});
