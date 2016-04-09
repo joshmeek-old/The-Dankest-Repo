6d6f648b3c5d02e87b13d40ee66116c3951c4955
Josh <josh@Joshs-MacBook-Air.local>
2016-04-08 23:48:30 -0400

    Started working in python

---
+++ src/test.js
@@ -0,0 +1,6 @@
+$.ajax({
+    url: 'api.reimaginebanking.com/atms?key=your_key',
+    success: function(results){
+        console.log("test")
+    }
+});
---
+++ src/test.py
@@ -0,0 +1,17 @@
+# This is written for PYTHON 3
+# Don't forget to install requests package
+
+import requests
+import json
+import urllib
+
+customerId = 'your customerId here'
+apiKey = 'your apiKey here'
+
+url = 'http://api.reimaginebanking.com/enterprise/merchants?key=9353cf37bda3beedf35c7964ea1e1f3d'#.format(customerId,apiKey)
+
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
